ac61e010b21c2a77a4bb79b5f9466f947fa85195
Lucas de Lacerda <106289445+Lucasdelacerda@users.noreply.github.com>
2026-01-20 01:06:08 -0300

    Projection

--- src/main/java/com/scrimet/dslist/services/GameService.java
+++ src/main/java/com/scrimet/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.scrimet.dslist.services;
 import com.scrimet.dslist.dto.GameDTO;
 import com.scrimet.dslist.dto.GameMinDTO;
 import com.scrimet.dslist.entities.Game;
+import com.scrimet.dslist.projections.GameMinProjection;
 import com.scrimet.dslist.repositories.GameRepository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,4 +28,11 @@ public class GameService {
         return result.stream().map(x -> new GameMinDTO(x)).toList();
 
     }
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+
+    }
+
 }
