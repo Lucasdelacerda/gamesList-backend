9883144d7011476c80474c839cf5ea8b8721b066
Lucas de Lacerda <106289445+Lucasdelacerda@users.noreply.github.com>
2026-02-22 23:21:47 -0300

    All games page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,48 @@
-import { useEffect, useState } from 'react'
-import api from './services/api'
+import ListGameContainer from "./components/ListGameContainer"
 import './App.css'
+import { useEffect, useState } from "react";
+import api from './services/api';
 
-
-interface Game {
-  id:number;
-  title:string;
-  year:number;
-  imgUrl:string;
-}
 
 function App() {
-const [games,setGames] = useState<Game[]>([]);
 
-useEffect(() =>{
-  api.get('/games')
-  .then(response =>{
-    console.log("Dados que chegaram do Java:", response.data);
-   setGames(response.data); 
-  })
-  .catch(err => console.error("erro na requisição", err));
-},[])
+  const [searchTerm, setSearchTerm] = useState('');
+  const [games, setGames] = useState([]);
 
+  const fetchAllGames = () => {
+    api.get("/games")
+      .then(response => setGames(response.data))
+      .catch(err => console.error(err));
+  }
+
+  const handleSearch = () => {
+  if (searchTerm === '') {
+
+    fetchAllGames();
+    return;    
+  }
+  
+    api.get('games/title', {
+      params: { title: searchTerm }
+    })
+      .then(response => {
+        setGames(response.data);
+      })
+      .catch(err => console.error("Erro ao filtrar", err)
+      )
+  }
+
+  useEffect(() =>{
+  fetchAllGames();  
+  },[])
   return (
-    <>
-      <input className='searchBar' type="text" />
-
-     
-      <ul>
-        {games?.map(game => (
-          <li key={game.id}>{game.title}</li>
-        ))}
-      </ul>
-    </>
+    <div>
+      <div className="search-bar">
+      <input className='searchBar' type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Nome do jogo" />
+      <button onClick={handleSearch}>Pesquisar</button>
+      </div>
+      <ListGameContainer games={games} />
+    </div>
   )
 }
 
