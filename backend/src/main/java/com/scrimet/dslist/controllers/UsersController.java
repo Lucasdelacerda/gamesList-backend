49a049fb2e8421c5edee5e615ba1be042d38775b
Lucas de Lacerda <106289445+Lucasdelacerda@users.noreply.github.com>
2026-03-29 16:34:59 +0000

    :bug:fix: security 401 liberated

--- backend/src/main/java/com/scrimet/dslist/controllers/UsersController.java
+++ backend/src/main/java/com/scrimet/dslist/controllers/UsersController.java
@@ -5,14 +5,9 @@ import com.scrimet.dslist.dto.UsersDTO;
 import com.scrimet.dslist.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -27,7 +22,7 @@ public class UsersController {
     public List<UsersDTO> searchByEmail(@RequestParam String email){
        return usersService.searchByEmail(email);
     }
-    @PostMapping("/users")
+    @PostMapping
     public UsersDTO insert(@RequestBody UsersDTO dto){
         dto = usersService.insert(dto);
         return dto;
